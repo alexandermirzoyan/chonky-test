56ee9197e00229ab3a333d1a7358aec3cd99767e
Alexandr Mirzoyan <alexandr.mirz12@gmail.com>
2020-08-04 18:37:53 +0400

    chonky test application

--- src/App.js
+++ src/App.js
@@ -1,25 +1,78 @@
-import React from 'react';
+import React,
+{
+  useState,
+  useCallback,
+  useMemo
+} from 'react';
 import logo from './logo.svg';
 import './App.css';
 
+import 'chonky/style/main.css';
+import {FileBrowser, FileList, FileSearch, FileToolbar, ChonkyActions} from 'chonky';
+
 function App() {
+  const [currentFolderId, setCurrentFolderId] = useState('xXre');
+  const [folderChain, setFolderChain] = useState(
+    [
+      {id: 'xXre', name: 'My Documents'},
+      {id: 'BtrE', name: 'Websites'},
+      {id: 'Asde', name: 'React'}
+    ]
+  );
+
+  /*const folderChain = React.useMemo(
+    () => [
+      { id: 'xXre', name: 'My Documents' },
+      { id: 'BtrE', name: 'Websites' },
+      { id: 'Asde', name: 'React' },
+    ],
+    []
+  );*/
+
+  const files = [
+    //null, // Will show loading animation
+    {id: 'xWbZ', name: 'Instructions.txt'},
+    {id: 'folderId', name: 'Tools', isDir: true},
+  ];
+
+  const fileActions = useMemo(
+    () => [
+      ChonkyActions.CreateFolder, // Adds a button to the toolbar
+      ChonkyActions.UploadFiles, // Adds a button
+      ChonkyActions.DownloadFiles, // Adds a button
+      ChonkyActions.CopyFiles, // Adds a button and a shortcut: Ctrl+C
+      ChonkyActions.DeleteFiles, // Adds a button and a shortcut: Delete
+    ],
+    []
+  );
+
+  const handleFileAction = useCallback(
+    (action, data) => {
+      if (data.actionId === 'open_files' && data.target.id === 'folderId') {
+        console.log(folderChain);
+
+        const {id, name} = data.target;
+        setFolderChain(oldFolderChain => [...oldFolderChain, {
+          id,
+          name
+        }]);
+      }
+      console.log(action);
+      console.log(data);
+    }
+  );
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <FileBrowser
+      fileActions={fileActions}
+      files={files}
+      folderChain={folderChain}
+      onFileAction={handleFileAction}
+    >
+      <FileToolbar/>
+      <FileSearch/>
+      <FileList/>
+    </FileBrowser>
   );
 }
 
